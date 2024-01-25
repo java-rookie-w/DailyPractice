fae4ea16094345aa5c3626ba3c673b4d5b071cd9
wangruike <2596564760@qq.com>
2024-01-25 19:54:31 +0800

    新加模块springboot

--- mianshi/src/main/java/org/wang/mianshi/collectiontest/ListTest.java
+++ mianshi/src/main/java/org/wang/mianshi/collectiontest/ListTest.java
@@ -4,22 +4,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class ListTest {
     public static void main(String[] args) {
 //        List<Integer> list = new ArrayList<>();
 //        list.add(2,1);
 
-//        List<Integer> list2 = new ArrayList<>(Collections.nCopies(10, 3));
-//        System.out.println(list2);
-//
-//        List<Integer> list3 = new LinkedList<>();
-//        // linkedlist默认使用尾插
-//        list3.add(1);
+        List<Integer> list2 = new ArrayList<>(Collections.nCopies(10, 3));
+        System.out.println(list2);
 
+        List<Integer> list3 = new LinkedList<>();
+        // linkedlist默认使用尾插
+        list3.add(1);
 
-       List<Integer> list4 = new ArrayList<>();
-        list4.add(1);
-        System.out.println(list4.get(8));
+        CopyOnWriteArrayList<String> cwa = new CopyOnWriteArrayList<>();
+        cwa.add("c");
+        cwa.get(0);
+
     }
 }
