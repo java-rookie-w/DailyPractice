36bb37cbf9e40a38a8d28b207e53c9cd84827449
wangruike <2596564760@qq.com>
2024-03-11 11:13:26 +0800

    xmind更新

---
+++ mianshi/src/main/java/org/wang/mianshi/theadtest/ThreadPoolExecutorTest.java
@@ -0,0 +1,37 @@
+package org.wang.mianshi.theadtest;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 线程池测试类
+ */
+public class ThreadPoolExecutorTest {
+
+    private static final int corePoolSize = 2;
+
+    private static int maximumPoolSize = 4;
+
+    private static long keepAliveTime = 1000;
+
+    private static TimeUnit unit = TimeUnit.SECONDS;
+
+    private static int queue_cap = 2;
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, unit, new ArrayBlockingQueue<>(queue_cap));
+
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            executor.execute(()->{
+                System.out.println("当前线程：" + finalI);
+                try {
+                    Thread.sleep(100000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+}
